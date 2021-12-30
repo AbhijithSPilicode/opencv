e599cf4ac6326a9b929d350729317c594609c151
Abhijith S <93380590+AbhijithSPilicode@users.noreply.github.com>
2021-12-30 19:44:17 +0530

    Update 36 trackbar in hsv.py

--- 36 trackbar in hsv.py
+++ 36 trackbar in hsv.py
@@ -1,24 +1,52 @@
-#trackbar in hsv
-import cv2
+
+#trackbar in HSV
 import numpy as np
-img=cv2.imread("C:\\Users\\user\\Desktop\\asap\\opencv images\\14.jpeg")
-hsv=cv2.cvtColor(img,cv2.COLOR_BGR2HSV) #hsv image
-#only blue color will take from it using bitwise operation
-def printVal(x):   #to get value from trackbar
-    print(x)
-l_b1=cv2.createTrackbar("l_b1","image",0,255,printVal) #name,window,start value,end value to
-l_b2=cv2.createTrackbar(switch,"image",0,100,printVal)
-l_b3=cv2.createTrackbar(switch,"image",0,100,printVal)
-u_b1=cv2.createTrackbar("u_b1","image",0,255,printVal) #name,window,start value,end value to
-u_b2=cv2.createTrackbar(switch,"image",0,255,printVal)
-u_b3=cv2.createTrackbar(switch,"image",0,255,printVal)
-l_b=np.array([l_b1,100,100]) #lowerbound of blue #finding using colorpicker from site AlloyUI #firstone/2,100,100
-u_b=np.array([u_b1,255,255]) #upperbound of blue #100 and 255 will be same for all colours
-mask=cv2.inRange(hsv,l_b,u_b) #mask of blue
-#if hsv and mask bitwise and done,it will give mask only,which is blue
-res=cv2.bitwise_and(img,img,mask=mask) #img as it is,2nd img is img+mask
-cv2.imshow("image",img)
-cv2.imshow("blue mask",mask)
-cv2.imshow("Output",res)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+import cv2
+
+def callback(x):
+ pass
+
+cap = cv2.VideoCapture(0)
+cv2.namedWindow('image')
+
+ilowH = 0
+ihighH = 179
+
+ilowS = 0
+ihighS = 255
+ilowV = 0
+ihighV = 255
+
+cv2.createTrackbar('lowH','image',ilowH,179,callback)
+cv2.createTrackbar('highH','image',ihighH,179,callback)
+
+cv2.createTrackbar('lowS','image',ilowS,255,callback)
+cv2.createTrackbar('highS','image',ihighS,255,callback)
+
+cv2.createTrackbar('lowV','image',ilowV,255,callback)
+cv2.createTrackbar('highV','image',ihighV,255,callback)
+
+while True:
+    # grab the frame
+    ret, frame = cap.read()
+
+    # get trackbar positions
+    ilowH = cv2.getTrackbarPos('lowH', 'image')
+    ihighH = cv2.getTrackbarPos('highH', 'image')
+    ilowS = cv2.getTrackbarPos('lowS', 'image')
+    ihighS = cv2.getTrackbarPos('highS', 'image')
+    ilowV = cv2.getTrackbarPos('lowV', 'image')
+    ihighV = cv2.getTrackbarPos('highV', 'image')
+
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    lower_hsv = np.array([ilowH, ilowS, ilowV])
+    higher_hsv = np.array([ihighH, ihighS, ihighV])
+    mask = cv2.inRange(hsv, lower_hsv, higher_hsv)
+
+    frame = cv2.bitwise_and(frame, frame, mask=mask)
+
+    # show thresholded image
+    cv2.imshow('image', frame)
+    k = cv2.waitKey(1000) & 0xFF # large wait time to remove freezing
+    if k == 113 or k == 27:
+        break
